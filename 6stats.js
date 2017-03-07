55fd54ca9cb593262555ed398b012b348bbc9f1d
Siobhan Hokin <skehokin@gmail.com>
2017-03-07 22:01:47 +1100

    add capitalising functions in new js file

--- 6stats.js
+++ 6stats.js
@@ -3,3 +3,29 @@ function capitalise(string){
 thing=string.charAt(0).toUpperCase() +string.slice(1);
 return thing;
 }
+
+function titlecaps(manyWords){
+  manyWords=capitalise(manyWords);
+  x=0;
+  while (x!=-1){
+    x=manyWords.indexOf(" ", x+1);
+    manyWords=manyWords.slice(0,x+1)+manyWords.charAt(x+1).toUpperCase()+manyWords.slice(x+2);
+  }
+  return manyWords;
+}
+function sentencecaps(sentences){
+  var newsentences="";
+var splitsentences=sentences.split('. ');
+
+for (var i = 0; i < splitsentences.length; i++) {
+  if (i!=splitsentences.length-1){
+newsentences=newsentences+capitalise(splitsentences[i])+". ";
+}
+else{
+  newsentences=newsentences+capitalise(splitsentences[i]);
+
+
+}
+}
+  return newsentences;
+}
